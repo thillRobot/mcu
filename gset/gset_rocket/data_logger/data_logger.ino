9083261da83bdd9b088de9b69ec7ac3fab794164
thill <thill@tntech.edu>
2021-04-21 00:25:47 -0500

    revised data logger for GSET rockets

--- gset/gset_rocket/data_logger/data_logger.ino
+++ gset/gset_rocket/data_logger/data_logger.ino
@@ -1,47 +1,140 @@
+
 /*
-  SD card datalogger
+  GSET - Data Acquisition Rocket - Summer 2021 - Tennessee Technological University
+  Tristan Hill - April 20, 2021
+  
+  This code began as example code from the Arduino library
+  The original example code 'DataLogger'  was modified for this project
+  created  24 Nov 2010
+  modified 9 Apr 2012
+  by Tom Igoe
 
- This example shows how to log data from three analog sensors
- to an SD card using the SD library.
+  Example code from the Arduino library BNO055 was also used. 
+  The example 'Bunny' has been modified for this project. 
+  
+  Modified by Tristan Hill - 06/24/2019 
+  To use this with the mega you have to use the wiring shown below
+  
+  Revised ny Tristan Hill - 04/20/2021
+  This year we switched to the MKR1010 Wifi board
 
- The circuit:
- * analog sensors on analog ins 0, 1, and 2
- * SD card attached to SPI bus as follows:
- ** MOSI - pin 11
- ** MISO - pin 12
- ** CLK - pin 13
- ** CS - pin 4 (for MKRZero SD: SDCARD_SS_PIN)
+  BN0055 - working - values look good, not validated - 04/20/2021
+  
+  SD Card Breakout - working - data in files look good, not validated - 04/20/2021
 
- created  24 Nov 2010
- modified 9 Apr 2012
- by Tom Igoe
+  
+  Caution:  This code is still a rough mashup of the two peices of example code, and much TLC is still need. 
+  
 
- This example code is in the public domain.
+  ***** BNO055 - i2c Sensor Board - info *****
+   
+  This driver uses the Adafruit unified sensor library (Adafruit_Sensor),
+  which provides a common 'type' for sensor data and some helper functions.
+  
+  To use this driver you will also need to download the Adafruit_Sensor
+  library and include it in your libraries folder.
+  
+  You should also assign a unique ID to this sensor for use with
+  the Adafruit Sensor API so that you can identify this particular
+  sensor in any data logs, etc.  To assign a unique ID, simply
+  provide an appropriate value in the constructor below (12345
+  is used by default in this example).
+  
+  Connections
+  ===========
+  Connect SCL to analog 5
+  Connect SDA to analog 4
+  Connect VDD to 3.3-5V DC
+  Connect GROUND to common ground
+  
 
- Modified by Tristan Hill - 06/24/2019 
- To use this with the mega you have to use the wiring shown below
+  ***** SD Card Breakout Board - info ***** 
 
- MEGA|MicroSD Breakout
- 
- 53-----CS
- 52-----CLK 
- 51-----D1
- 50-----D0
- 
- 5v-----5v
+  The circuit:
+  * SD card attached to SPI bus as follows:
+  ** MOSI - pin 11
+  ** MISO - pin 12
+  ** CLK - pin 13
+  ** CS - pin 4 (for MKRZero SD: SDCARD_SS_PIN)
+
+  ***** Wiring Diagram ***** 
+  
+  MEGA | MicroSD Breakout (not currently used)
+  
+  53-----CS
+  52-----CLK 
+  51-----D1
+  50-----D0
+  
+  5v-----5v
     (nc)3v 
- Gnd---Gnd
+  Gnd---Gnd
+  
+  MKR1010 | MicroSD Breakout (currently used)
+  
+  MISO(10)-----CS
+  SCK (9)-----CLK 
+  MOSI(8)-----D1
+  GPIO(7)-----D0 (you choose this pin)
+  
+  5v-----5v
+    (nc)3v 
+  Gnd---Gnd
+  
+  MKR1010 | BNO055 Sensor Board
+  
+  SDA (11)-----SDA
+  SCL (12)-----SCL
+  
+  5v----Vin
+    (nc)3v 
+  Gnd---Gnd
  
- */
+  
+*/
+
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
 
 #include <SPI.h>
 #include <SD.h>
 
 const int chipSelect = 53;
 
+/* Set the delay between fresh samples */
+#define BNO055_SAMPLERATE_DELAY_MS (100)
+
+// Check I2C device address and correct line below (by default address is 0x29 or 0x28)
+//                                   id, address
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
+
+/**************************************************************************/
+/*
+    Displays some basic information on this sensor from the unified
+    sensor API sensor_t type (see Adafruit_Sensor for more information)
+*/
+/**************************************************************************/
+void displaySensorDetails(void)
+{
+  sensor_t sensor;
+  bno.getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" xxx");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" xxx");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" xxx");
+  Serial.println("------------------------------------");
+  Serial.println("");
+  delay(500);
+}
+
 void setup() {
   // Open serial communications and wait for port to open:
-  Serial.begin(9600);
+  Serial.begin(11520);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
@@ -56,20 +149,83 @@ void setup() {
     while (1);
   }
   Serial.println("card initialized.");
+
+  //Serial.begin(115200);
+  Serial.println("Orientation Sensor Test"); Serial.println("");
+
+  /* Initialise the sensor */
+  if(!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+   
+  delay(1000);
+
+  /* Use external crystal for better accuracy */
+  bno.setExtCrystalUse(true);
+   
+  /* Display some basic information on this sensor */
+  displaySensorDetails();
 }
 
 void loop() {
+
+  /* Get a new sensor event */
+  sensors_event_t event;
+  bno.getEvent(&event);
+
+  /* Board layout:
+         +----------+
+         |         *| RST   PITCH  ROLL  HEADING
+     ADR |*        *| SCL
+     INT |*        *| SDA     ^            /->
+     PS1 |*        *| GND     |            |
+     PS0 |*        *| 3VO     Y    Z-->    \-X
+         |         *| VIN
+         +----------+
+  */
+
+  /* The processing sketch expects data as roll, pitch, heading */
+  Serial.print(F("Orientation: "));
+  Serial.print((float)event.orientation.x);
+  Serial.print(F(" "));
+  Serial.print((float)event.orientation.y);
+  Serial.print(F(" "));
+  Serial.print((float)event.orientation.z);
+  Serial.println(F(""));
+
+  /* Also send calibration data for each sensor. */
+  uint8_t sys, gyro, accel, mag = 0;
+  bno.getCalibration(&sys, &gyro, &accel, &mag);
+  Serial.print(F("Calibration: "));
+  Serial.print(sys, DEC);
+  Serial.print(F(" "));
+  Serial.print(gyro, DEC);
+  Serial.print(F(" "));
+  Serial.print(accel, DEC);
+  Serial.print(F(" "));
+  Serial.println(mag, DEC);
+
+  delay(BNO055_SAMPLERATE_DELAY_MS);
+  
   // make a string for assembling the data to log:
   String dataString = "";
 
   // read three sensors and append to the string:
-  for (int analogPin = 0; analogPin < 3; analogPin++) {
-    int sensor = analogRead(analogPin);
-    dataString += String(sensor);
-    if (analogPin < 2) {
-      dataString += ",";
-    }
-  }
+  //for (int analogPin = 0; analogPin < 3; analogPin++) {
+    //int sensor = analogRead(analogPin);
+    //int sensor = analogRead(analogPin);
+    
+  dataString += String(event.orientation.x);
+  dataString += ",";
+  dataString += String(event.orientation.y);
+  dataString += ",";
+  dataString += String(event.orientation.z);
+  dataString += ",";
+   
+  //}
 
   // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
