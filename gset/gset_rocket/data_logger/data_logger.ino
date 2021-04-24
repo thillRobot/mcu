3e7b458267a0310d012fb88f7dd9913598491e83
thill <thill@tntech.edu>
2021-04-24 13:25:17 -0500

    added file number and entry number to GSET data_logger

--- gset/gset_rocket/data_logger/data_logger.ino
+++ gset/gset_rocket/data_logger/data_logger.ino
@@ -24,6 +24,8 @@
 
   
   Caution:  This code is still a rough mashup of the two peices of example code, and much TLC is still need. 
+
+  - I have added all the sensors as shown in the example code "read_all_data"
   
 
   ***** BNO055 - i2c Sensor Board - info *****
@@ -61,10 +63,10 @@
   
   MEGA | MicroSD Breakout (not currently used)
   
-  53-----CS
-  52-----CLK 
-  51-----D1
-  50-----D0
+  SS(53)-------CS
+  SCK(52)-----CLK 
+  MOSI(51)-----D1
+  MISO(50)-----D0
   
   5v-----5v
     (nc)3v 
@@ -72,10 +74,12 @@
   
   MKR1010 | MicroSD Breakout (currently used)
   
-  MISO(10)-----CS
-  SCK (9)-----CLK 
+  GPIO(7)-----CS
   MOSI(8)-----D1
-  GPIO(7)-----D0 (you choose this pin)
+  SCK(9)-----CLK
+  MISO(10-----D0 
+  
+  MISO(10)-----D0 (you choose this pin)
   
   5v-----5v
     (nc)3v 
@@ -112,7 +116,11 @@
 #include <SPI.h>
 #include <SD.h>
 
-const int chipSelect = 53;
+//const int chipSelect = 53; // used on MEGA
+const int chipSelect = 7; // used on MKR , not setting this  can cause the SD to write to ALMOST work
+int entry_number = 0;
+int file_number = 0;
+String file_string;
 
 /* Set the delay between fresh samples */
 #define BNO055_SAMPLERATE_DELAY_MS (100)
@@ -179,59 +187,137 @@ void setup() {
    
   /* Display some basic information on this sensor */
   displaySensorDetails();
+
+  file_string="datalog";
+  file_string+= String(file_number);
+  file_string+=".txt";
+  
+
+  
 }
 
 void loop() {
 
+  //could add VECTOR_ACCELEROMETER, VECTOR_MAGNETOMETER,VECTOR_GRAVITY...
+  sensors_event_t orientationData , angVelocityData , linearAccelData, magnetometerData, accelerometerData, gravityData;
+  bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
+  bno.getEvent(&angVelocityData, Adafruit_BNO055::VECTOR_GYROSCOPE);
+  bno.getEvent(&linearAccelData, Adafruit_BNO055::VECTOR_LINEARACCEL);
+  bno.getEvent(&magnetometerData, Adafruit_BNO055::VECTOR_MAGNETOMETER);
+  bno.getEvent(&accelerometerData, Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  bno.getEvent(&gravityData, Adafruit_BNO055::VECTOR_GRAVITY);
+
+  printEvent(&orientationData);
+  printEvent(&angVelocityData);
+  printEvent(&linearAccelData);
+  printEvent(&magnetometerData);
+  printEvent(&accelerometerData);
+  printEvent(&gravityData);
+
   /* Get a new sensor event */
-  sensors_event_t event;
-  bno.getEvent(&event);
+  //sensors_event_t event;
+  //bno.getEvent(&event);
 
-
-
-  /* The processing sketch expects data as roll, pitch, heading */
-  Serial.print(F("Orientation: "));
-  Serial.print((float)event.orientation.x);
-  Serial.print(F(" "));
-  Serial.print((float)event.orientation.y);
-  Serial.print(F(" "));
-  Serial.print((float)event.orientation.z);
-  Serial.println(F(""));
-
-  /* Also send calibration data for each sensor. */
-  uint8_t sys, gyro, accel, mag = 0;
-  bno.getCalibration(&sys, &gyro, &accel, &mag);
-  Serial.print(F("Calibration: "));
-  Serial.print(sys, DEC);
-  Serial.print(F(" "));
-  Serial.print(gyro, DEC);
-  Serial.print(F(" "));
-  Serial.print(accel, DEC);
-  Serial.print(F(" "));
-  Serial.println(mag, DEC);
-
+  int8_t boardTemp = bno.getTemp();
+  Serial.println();
+  Serial.print(F("temperature: "));
+  Serial.println(boardTemp);
+  /*
+  uint8_t system, gyro, accel, mag = 0;
+  bno.getCalibration(&system, &gyro, &accel, &mag);
+  Serial.println();
+  Serial.print("Calibration: Sys=");
+  Serial.print(system);
+  Serial.print(" Gyro=");
+  Serial.print(gyro);
+  Serial.print(" Accel=");
+  Serial.print(accel);
+  Serial.print(" Mag=");
+  Serial.println(mag);
+  */
+  
+  Serial.println("--");
   delay(BNO055_SAMPLERATE_DELAY_MS);
   
-  // make a string for assembling the data to log:
-  String dataString = "";
+}
 
-  // read three sensors and append to the string:
-  //for (int analogPin = 0; analogPin < 3; analogPin++) {
-    //int sensor = analogRead(analogPin);
-    //int sensor = analogRead(analogPin);
-    
-  dataString += String(event.orientation.x);
+void printEvent(sensors_event_t* event) {
+  double x = -1000000, y = -1000000 , z = -1000000; //dumb values, easy to spot problem
+
+  // make a string for assembling the data to log:
+  String dataString = "Data Log Entry Number:";
+  dataString += String(entry_number);
+  
+  if (event->type == SENSOR_TYPE_ACCELEROMETER) {
+    Serial.print("Accl:");
+    x = event->acceleration.x;
+    y = event->acceleration.y;
+    z = event->acceleration.z;
+    dataString += "Accl:";
+  }
+  else if (event->type == SENSOR_TYPE_ORIENTATION) {
+    Serial.print("Orient:");
+    x = event->orientation.x;
+    y = event->orientation.y;
+    z = event->orientation.z;
+    dataString += "Orient:";
+  }
+  else if (event->type == SENSOR_TYPE_MAGNETIC_FIELD) {
+    Serial.print("Mag:");
+    x = event->magnetic.x;
+    y = event->magnetic.y;
+    z = event->magnetic.z;
+    dataString += "Mag:";
+  }
+  else if (event->type == SENSOR_TYPE_GYROSCOPE) {
+    Serial.print("Gyro:");
+    x = event->gyro.x;
+    y = event->gyro.y;
+    z = event->gyro.z;
+    dataString += "Gyro:";
+  }
+  else if (event->type == SENSOR_TYPE_ROTATION_VECTOR) {
+    Serial.print("Rot:");
+    x = event->gyro.x;
+    y = event->gyro.y;
+    z = event->gyro.z;
+    dataString += "Rot:";
+  }
+  else if (event->type == SENSOR_TYPE_LINEAR_ACCELERATION) {
+    Serial.print("Linear:");
+    x = event->acceleration.x;
+    y = event->acceleration.y;
+    z = event->acceleration.z;
+    dataString += "Linear:";
+  }
+  else {
+    Serial.print("Unk:");
+    dataString += "Unk:";
+  }
+  
+
+  dataString += String(x);
   dataString += ",";
-  dataString += String(event.orientation.y);
+  dataString += String(y);
   dataString += ",";
-  dataString += String(event.orientation.z);
+  dataString += String(z);
   dataString += ",";
-   
-  //}
+  
+
+  /*
+  Serial.print("\tx= ");
+  Serial.print(x);
+  Serial.print(" |\ty= ");
+  Serial.print(y);
+  Serial.print(" |\tz= ");
+  Serial.println(z);
+  */
+  
+  //dataString="Test";
 
   // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
-  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+  File dataFile = SD.open(file_string, FILE_WRITE);
 
   // if the file is available, write to it:
   if (dataFile) {
@@ -242,6 +328,9 @@ void loop() {
   }
   // if the file isn't open, pop up an error:
   else {
-    Serial.println("error opening datalog.txt");
+    Serial.println("error opening datalog file:");
+    Serial.println(file_string);
   }
+
+  entry_number++;
 }
